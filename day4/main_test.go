7daf6a33a59666d2a3e841c32f6ff3218c2daed9
Mizael Pinto Almeida <mzalmeida.a@gmail.com>
2023-12-06 18:44:25 -0300

    feat: tests for challenge 2

--- day4/main_test.go
+++ day4/main_test.go
@@ -15,3 +15,13 @@ func TestExampleInput__Challenge1(t *testing.T) {
 		t.Errorf("Failed challenge 1: expected %d, got %d", expected, result)
 	}
 }
+
+func TestExampleInput__Challenge2(t *testing.T) {
+	expected := 30
+
+	result := countCopies(cEXAMPLE_FILE)
+
+	if result != expected {
+		t.Errorf("Failed challenge 2: expected: %d, got %d", expected, result)
+	}
+}
