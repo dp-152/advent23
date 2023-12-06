39ef86a2d40ce3a5a840b441a2263fdcc87a33c7
Mizael Pinto Almeida <mzalmeida.a@gmail.com>
2023-12-06 16:53:39 -0300

    feat: tests for challenge 2

--- day3/main_test.go
+++ day3/main_test.go
@@ -16,3 +16,14 @@ func TestExampleInput__Challenge1(t *testing.T) {
 		t.Errorf("Failed challenge 1: expected %d, got %d", expected, result)
 	}
 }
+
+func TestExampleInput__Challenge2(t *testing.T) {
+	expected := 467835
+	engine := loadEngineSchematic(cEXAMPLE_FILE)
+
+	result := sumGearRatios(engine)
+
+	if result != expected {
+		t.Errorf("Failed challenge 2: expected %d, got %d", expected, result)
+	}
+}
