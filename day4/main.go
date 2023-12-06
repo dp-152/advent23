8bb16024d86ab734277fff2eaba27909a770ecdc
Mizael Pinto Almeida <mzalmeida.a@gmail.com>
2023-12-06 18:44:15 -0300

    feat: challenge part 2

--- day4/main.go
+++ day4/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 
 	"github.com/dp-152/advent23/common/fs"
 )
@@ -10,28 +11,46 @@ type lotteryCard struct {
 	id      int
 	winNums map[int]bool
 	ownNums map[int]bool
+	matchCt *int
 }
 
 const cCARD_SUFFIX = ':'
 const cNUMGROUP_SEPARATOR = '|'
 
-func (c *lotteryCard) score() (score int) {
-	for num := range c.ownNums {
-		if _, ok := c.winNums[num]; ok {
-			if score == 0 {
-				score = 1
-			} else {
-				score *= 2
+func (c *lotteryCard) score() int {
+	if c.matchCt == nil {
+		c.matches()
+	}
+
+	if *c.matchCt == 0 {
+		return 0
+	}
+
+	return int(math.Pow(2, float64(*c.matchCt-1)))
+}
+
+func (c *lotteryCard) matches() int {
+	if c.matchCt == nil {
+		matchCount := 0
+		for num := range c.ownNums {
+			if _, ok := c.winNums[num]; ok {
+				matchCount += 1
 			}
 		}
+		c.matchCt = &matchCount
 	}
-	return
+
+	return *c.matchCt
 }
 
 func main() {
 	pointsSum := sumPoints("input.txt")
 
-	fmt.Printf("Sum of lottery cards: %d", pointsSum)
+	fmt.Printf("Sum of lottery card points: %d\n", pointsSum)
+
+	copies := countCopies("input.txt")
+
+	fmt.Printf("Sum of all cards: %d\n", copies)
 }
 
 func sumPoints(inputFile string) (sum int) {
@@ -43,6 +62,29 @@ func sumPoints(inputFile string) (sum int) {
 		sum += card.score()
 	}
 
+	return
+}
+
+func countCopies(inputFile string) (count int) {
+	cards := make([]*lotteryCard, 0, 100)
+	cardCounts := make(map[int]int)
+
+	cancelChan := make(chan struct{})
+	fileChan := fs.ReadLines(inputFile, cancelChan)
+
+	for line := range fileChan {
+		card := parseLine(line)
+		cards = append(cards, card)
+		cardCounts[card.id] = 1
+	}
+
+	for _, card := range cards {
+		count += cardCounts[card.id]
+		matches := card.matches()
+		for copyOffset := 1; copyOffset <= matches; copyOffset += 1 {
+			cardCounts[card.id+copyOffset] += cardCounts[card.id]
+		}
+	}
 	return
 }
 
